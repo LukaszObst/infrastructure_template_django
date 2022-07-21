6a8b24c8267ea4cf023db08bbf7d169bdeafd400
Lukasz Obst <info@loukas.tech>
2022-07-21 22:25:16 +0200

    build workflow

--- webapp/entrypoint.sh
+++ webapp/entrypoint.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 
-echo "Initializing postgres db..."
+echo "Initializing db..."
 
 while ! nc -z ${DJANGO_DB_HOST} ${DJANGO_DB_PORT}; do
   sleep 1
+  echo "Waiting for db..."
 done
 
-echo "postgres database has initialized successfully"
+echo "database has initialized successfully"
 
-python manage.py makemigrations
-python manage.py migrate --noinput
-python manage.py collectstatic --noinput
-
-if [ ${DJANGO_SUPERUSER_USERNAME} ]
-then
-  echo "Creating superuser"
-  python manage.py createsuperuser --noinput 
-fi
-
+exec "$@"
 gunicorn ${DJANGO_PROJECT_SUBDIR}.wsgi:application --bind 0.0.0.0:${WEBAPP_PORT}
-#exec "$@"
--- webapp/loukas_tech/settings.py
+++ webapp/loukas_tech/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 
 
-ALLOWED_HOSTS = [ os.environ.get('ALLOWED_HOSTS') ]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(" ")
 CSRF_TRUSTED_ORIGINS = ["http://" + i for i in ALLOWED_HOSTS]
 
 SESSION_COOKIE_SECURE = True
